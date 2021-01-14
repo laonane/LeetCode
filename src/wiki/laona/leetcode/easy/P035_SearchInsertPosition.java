d98a955de9f383c308742dc23e57c032c9323d8d
HuaiAnGG <971375353@qq.com>
2021-01-14 23:51:26 +0800

    35. 搜索插入位置

---
+++ src/wiki/laona/leetcode/easy/P035_SearchInsertPosition.java
@@ -0,0 +1,9 @@
+package wiki.laona.leetcode.easy;
+/**
+ * @program: LeetCode
+ * @description: 35. 搜索插入位置
+ * @author: HuaiAnGG
+ * @create: 2021-01-14 22:23
+ **/
+public class P035_SearchInsertPosition {
+}
